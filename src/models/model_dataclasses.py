e226fd62c7735b7b357e9ba24dee6222a27da4d3
polsti <polina.stina@ktu.edu>
2024-04-23 04:55:57 +0300

    changed for release date from str to date

--- src/models/model_dataclasses.py
+++ src/models/model_dataclasses.py
@@ -3,62 +3,6 @@ from datetime import date
 from typing import Tuple, Any
 
 
-@dataclass
-class Country:
-
-    country_id: int
-    name: str
-    capital: str
-    region: str
-    population: int
-    area: int
-    phone_code: int
-    country_code: str
-    currency: str
-
-    def get_columns(self) -> Tuple[str, ...]:
-        
-        return tuple(column.name for column in fields(self))
-
-    def get_data(self) -> Tuple[Any, ...]:
-        
-        return tuple(getattr(self, column.name) for column in fields(self))
-
-
-# new class 
-
-@dataclass
-class Test:
-
-# here should be properties for a specific table 
-# for TV Shows - some id, name, descriptio and bla bla bla
-# if something is null
-# if integer is nullable no change
-# if string  -  name: str | None # nullable
-    country_id: int
-    name: str
-    capital: str
-    region: str
-    population: int
-    area: int
-    phone_code: int
-    country_code: str
-    currency: str
-
-# no changes here
-    def get_columns(self) -> Tuple[str, ...]:
-        
-        return tuple(column.name for column in fields(self))
-
-    def get_data(self) -> Tuple[Any, ...]:
-        
-        return tuple(getattr(self, column.name) for column in fields(self))
-    
-    # add validation functions
-    # example 
-    # if self.capital contains number => error
-
-
 
 # class for a single TV Show
 @dataclass
@@ -73,7 +17,7 @@ class TV_Show:
     id_TV_Show : int
     title: str
     description : str
-    release_date: str # date  # TODO
+    release_date: date 
 
 # no changes here
     def get_columns(self) -> Tuple[str, ...]:
@@ -95,7 +39,7 @@ class Season:
     fk_TV_Showid_TV_Show : int # fk_TV_Showid_TV_Show
     title: str
     description: str
-    release_date: str # date  # TODO
+    release_date: date 
 
 # no changes here
     def get_columns(self) -> Tuple[str, ...]:
